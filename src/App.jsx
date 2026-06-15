a3408468da2e7f07937b68b39a6acbedc692a7ca
Lucas <lquevedoberjano@gmail.com>
2026-06-15 11:44:07 -0300

    chore: disable maintenance mode

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contacto from './pages/Contacto'
 import PortalCreditos from './pages/PortalCreditos'
 import Mantenimiento from './pages/Mantenimiento'
 
-const MANTENIMIENTO = true
+const MANTENIMIENTO = false
 
 export default function App() {
   if (MANTENIMIENTO) return <Mantenimiento />
